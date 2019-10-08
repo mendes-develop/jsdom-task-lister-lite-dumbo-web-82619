79d5dc98d5bcb10799ea958e5b976757a0b247e3
github username <github email address>
2019-10-08 19:04:57 -0400

    Added list items by priority with select tags

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let toDoUl = document.querySelector("#tasks")
+  let form = document.querySelector('#create-task-form')
+
+
+  addSelectToForm()
+
+  toDoUl.addEventListener("click", function(evt){
+      if (evt.target.classList.contains("taskItem")) {
+        evt.target.remove()
+      }
+
+  })
+
+  form.addEventListener("submit", function(evt){
+    evt.preventDefault()
+    let textInput = evt.target["new-task-description"].value
+    let priorityLi = evt.target.querySelector('select').value
+    console.log(priorityLi)
+    addToUl(textInput, priorityLi)
+  })
+
+
+  function addToUl(string, priority){
+    let newLi = document.createElement('li')
+    newLi.className = "taskItem " + priority
+    newLi.innerText = string
+    toDoUl.append(newLi)
+  }
+
+  function addSelectToForm(){
+    let collection = document.createElement('select')
+    let label = document.createElement('label')
+    label.innerHTML += "Task Priority:"
+    collection.innerHTML += "<option value='urgent'>Urgent</option> <option value='normal'>Normal</option> <option value='low'>Low</option>"
+    form.append(label)
+    form.append(collection)
+  }
+
+
+
+
+
+
+
 });
